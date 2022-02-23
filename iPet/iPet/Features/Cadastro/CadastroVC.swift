89af8367d161013a29344cbee8d1ec8ba95e81d7
devbrunolopes <brunolopes0203@gmail.com>
2022-02-23 18:37:19 -0300

    Criação da feature de cadastro, só faltam as lógicas dos botões

---
+++ iPet/iPet/Features/Cadastro/CadastroVC.swift
@@ -0,0 +1,38 @@
+//
+//  CadastroVC.swift
+//  iPet
+//
+//  Created by Bruno Lopes on 23/02/22.
+//
+
+import UIKit
+
+class CadastroVC: UIViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var iPetLabel: UILabel!
+    @IBOutlet weak var sloganLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var checkPasswordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var dontHaveAnAccountLabel: UILabel!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapBackButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapSignUpButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapSignInButton(_ sender: UIButton) {
+    }
+    
+    
+}

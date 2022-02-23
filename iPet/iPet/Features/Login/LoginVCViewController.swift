7db6873bc4ccd3e0112ff28ec10d12979a3feb5f
devbrunolopes <brunolopes0203@gmail.com>
2022-02-23 18:12:49 -0300

    Commit inicial de feature de Login, a Main.storyboard foi excluída e fiz a configuração pro app iniciar na LoginVC.storyboard

---
+++ iPet/iPet/Features/Login/LoginVCViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVCViewController.swift
+//  iPet
+//
+//  Created by Bruno Lopes on 23/02/22.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}

f25fbdc57fe48228d4342e87d7b58aa028c3a57c
devbrunolopes <brunolopes0203@gmail.com>
2022-03-09 22:10:00 -0300

    Commit inicial da Feature Home. Criei a tela e as ligações.

---
+++ iPet/iPet/Features/Home/Cell/HomeCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCell.swift
+//  iPet
+//
+//  Created by Bruno Lopes on 09/03/22.
+//
+
+import UIKit
+
+class HomeCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ iPet/iPet/Features/Home/HomeVC.swift
@@ -0,0 +1,25 @@
+//
+//  HomeVC.swift
+//  iPet
+//
+//  Created by Bruno Lopes on 09/03/22.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+  
+}

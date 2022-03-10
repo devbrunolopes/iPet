52c95b7c84e978382beb187d4ced79d77c8c0381
devbrunolopes <brunolopes0203@gmail.com>
2022-03-09 22:33:34 -0300

    Criei os métodos UICollectionViewDelegate, UICollectionViewDataSource e UICollectionViewDelegateFlowLayout

--- iPet/iPet/Features/Home/HomeVC.swift
+++ iPet/iPet/Features/Home/HomeVC.swift
@@ -18,8 +18,48 @@ class HomeVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.userImageView.layer.cornerRadius = userImageView.frame.size.height / 2
+        
+        self.collectionView.register(HomeCell.nib(), forCellWithReuseIdentifier: HomeCell.identifier)
+        
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
         
     }
   
+}
+
+//MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+
+extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCell.identifier, for: indexPath) as? HomeCell
+        
+        return cell ?? UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("\(indexPath)")
+    }
+   
+}
+
+//MARK: - UICollectionViewDelegateFlowLayout
+
+extension HomeVC: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.size.width / 2.3 , height: 150)
+    }
+    
 }

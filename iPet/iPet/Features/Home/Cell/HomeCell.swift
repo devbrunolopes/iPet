6c557d4483bd17e2f46a2e5feede75bcee2ee986
devbrunolopes <brunolopes0203@gmail.com>
2022-03-09 22:22:55 -0300

    Criação da célula

--- iPet/iPet/Features/Home/Cell/HomeCell.swift
+++ iPet/iPet/Features/Home/Cell/HomeCell.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class HomeCell: UICollectionViewCell {
-
+    
+    static let identifier: String = "HomeCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: self.identifier, bundle: nil)
+    }
+    
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var featureLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        view.layer.cornerRadius = 15
+        
     }
 
 }
